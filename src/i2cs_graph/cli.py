a88b6aff03fdf1f72316cc91ac0774d115c4bf3e
Vasili Vasilyeu <vasili.v@tut.by>
2025-09-14 13:26:31 -0700

    Fix "als-resolution" type and range

--- src/i2cs_graph/cli.py
+++ src/i2cs_graph/cli.py
@@ -2,9 +2,10 @@
 
 import sys
 import argparse
+import time
 
 from .error import Error
-from .read import read, ALS_DEFAULT_RESOLUTION
+from .read import read, ALS_SENSITIVITY, ALS_DEFAULT_RESOLUTION
 
 def make_args_parser() -> argparse.ArgumentParser:
     """ Creates an argument parser """
@@ -12,9 +13,10 @@ def make_args_parser() -> argparse.ArgumentParser:
     parser.add_argument('data', type=str, nargs=1,
                         help='file with CSV formatted data from i2cs-test script',
                         metavar='PATH')
-    parser.add_argument('--als-resolution', type=float, default=ALS_DEFAULT_RESOLUTION,
+    parser.add_argument('--als-resolution', type=int,
+                        choices=ALS_SENSITIVITY.keys(), default=ALS_DEFAULT_RESOLUTION,
                         help= 'resolution configured for ambient light sensor during measurements '
-                             f'(default: {ALS_DEFAULT_RESOLUTION})',
+                             f'(16 - 20, default: {ALS_DEFAULT_RESOLUTION})',
                         metavar='N')
     return parser
 
@@ -23,8 +25,9 @@ def main() -> int:
     args = make_args_parser().parse_args()
 
     try:
+        start = time.monotonic()
         data = read(args.data[0], args)
-        print(f'Got {len(data[0])} data points')
+        print(f'Got {len(data[0])} data points in {time.monotonic() - start:.1f}s')
     except Error as e:
         print(f'{e}. Exiting...', file=sys.stderr)
         return 1
